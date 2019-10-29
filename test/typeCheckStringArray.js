10d74d3fcd01cf9d6824606eafd3ad95ce78d8e1
Oumar Ly <lyomrau@gmail.com>
2019-10-29 16:17:44 -0400

    add coverage to source-config

---
+++ test/typeCheckStringArray.js
@@ -0,0 +1,34 @@
+/* eslint-env mocha */
+const assert = require('assert')
+
+let schema
+let sourceConfig
+
+describe('should type check an array of strings', function () {
+  beforeEach(function (done) {
+    sourceConfig = require('../sourceConfig')
+    sourceConfig.configs = {}
+    schema = {
+      allStrings: {
+        default: ['var1', 'var2', 'var3']
+      },
+      shouldTypecast: {
+        default: ['1', 'true', 'var3']
+      }
+    }
+    done()
+  })
+
+  it('should not typecast entry from all strings', function (done) {
+    sourceConfig(schema)
+
+    assert.deepStrictEqual(sourceConfig.configs.allStrings, ['var1', 'var2', 'var3'])
+    done()
+  })
+
+  it('should typecast entry to corresponding types', function (done) {
+    sourceConfig(schema)
+    assert.deepStrictEqual(sourceConfig.configs.shouldTypecast, [1, true, 'var3'])
+    done()
+  })
+})
