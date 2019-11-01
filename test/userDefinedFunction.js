92b49f0e6ddbb212c75ccc4c490039785e68745a
Oumar Ly <lyomrau@gmail.com>
2019-11-01 16:48:23 -0400

    Increase Test Coverage (#123)
    * add coverage to source-config
    
    * change test heading
    
    * 100% coverage
    
    Co-authored-by: Benjamin Lannon <benjamin@lannonbr.com>

---
+++ test/userDefinedFunction.js
@@ -0,0 +1,34 @@
+/* eslint-env mocha */
+const assert = require('assert')
+
+let schema
+let sourceConfig
+
+describe('User defined functions', function () {
+  beforeEach(function (done) {
+    sourceConfig = require('../sourceConfig')
+    sourceConfig.configs = {}
+    schema = {
+      userFunction: 'user defined function'
+    }
+    done()
+  })
+
+  it('should expect user defined function is true', function (done) {
+    schema.userFunction = function () {
+      return true
+    }
+
+    sourceConfig(schema)
+
+    assert.deepStrictEqual(sourceConfig.configs.userFunction, true)
+    done()
+  })
+
+  it('should expect user defined function is undefined', function (done) {
+    sourceConfig(schema)
+
+    assert.deepStrictEqual(sourceConfig.configs.userFunction, null)
+    done()
+  })
+})
